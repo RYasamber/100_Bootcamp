66b19be44c76d885d22a895526a083f3bdc241a8
yasamber rai <yasamber05@gmail.com>
2025-11-18 22:03:30 +0545

    Day-6 complete

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,6 @@
+def get_user_name_and_age():
+    name = input("What is your name?\n>> ")
+    age = input("How old are you?\n>> ")
+    print(f"I see - you are {name}, who is {age} yrs old.")
+print("Hello there!")
+get_user_name_and_age()
--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,32 @@
+import random
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+correct_ltr = []
+# TODO-1: - Use a while loop to let the user guess again.
+game_over = True
+while game_over:
+    display = ""
+    guess = input("Guess a letter: ").lower()
+    # TODO-2: Change the for loop so that you keep the previous correct letters in display.
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+    for letter in chosen_word:
+        if letter in correct_ltr:
+            display += letter
+        elif letter == guess:
+            display += letter
+            correct_ltr.append(letter)
+        else:
+            display += "_"
+
+    print(display)
+    if "_" not in display:
+        game_over = False
+        print("You Win!!")
